a4f6ec2154c02e5b4e28f43bcea337197cb53b80
Paul L West <plwest.atmark@gmail.com>
2019-09-20 13:34:31 +0900

    Updated 09_Inheritance with the basic Animal examle.

---
+++ 09_Inheritance/09_Inheritance/Inheritence/Animals/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_Inheritance.Animals
+{
+    class Dog : Animal  // See the "Cat" class for details
+    {
+        public override void MakeAnimalSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+        public override void PerformAnimalAction()
+        {
+            Console.WriteLine("Chase Cat.");
+        }
+    }
+}
--- 09_Inheritance/09_Inheritance/Program.cs
+++ 09_Inheritance/09_Inheritance/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _09_Inheritance.Animals;
+using System;
+using System.Collections.Generic;
 
 namespace _09_Inheritance
 {
@@ -6,7 +8,38 @@ namespace _09_Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //? We have a couple of ways to create lists with complex objects.
+            //?     This is a standard way since usually we don't know exactly what we
+            //?     need for a list when it's first created. However, if we DO know
+            //?     what we need when we create the list, we will "instantiate" the
+            //?     list with values right away.
+
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Cat());
+            animals.Add(new Dog());
+            animals.Add(new Horse());
+
+            //? This is a good way to instantiate. It's easy to read.
+            //?     If you want to use this method for your list creation, there
+            //?     no problem with doing it this way.
+            //List<Animal> animals = new List<Animal>
+            //{
+            //    new Cat(),
+            //    new Dog(),
+            //    new Horse()
+            //};
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"-------------- {animal.AnimalName} --------------");
+
+                animal.MakeAnimalSound();
+                animal.PerformAnimalAction();
+                animal.Sleep();
+                Console.WriteLine();
+            }
+
         }
     }
 }
