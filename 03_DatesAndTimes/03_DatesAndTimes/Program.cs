ea52b602a35fe94528dd0ed6c80234cca73bc032
Paul L West <plwest.atmark@gmail.com>
2019-09-21 15:40:50 +0900

    Updated 03_DateAndTimes. Updated 04_DecisionMaking

--- 03_DatesAndTimes/03_DatesAndTimes/Program.cs
+++ 03_DatesAndTimes/03_DatesAndTimes/Program.cs
@@ -6,7 +6,100 @@ namespace _03_DatesAndTimes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Dates and Times
+
+            #region The DateTime Type
+
+            //var currentTime = DateTime.Now; // current time
+            //var today = DateTime.Today; // current date - time is midnight
+            //var someDate = new DateTime(2016, 7, 1); // 1 July 2016, midnight
+            //var someMoment = new DateTime(2016, 7, 1, 8, 0, 0); // 1 July 2016, 08:00.00
+            //var tomorrow = DateTime.Today.AddDays(1);
+            //var yesterday = DateTime.Today.AddDays(-1);
+            //var someDay = DateTime.Parse("7/1/2016");
+
+            //Console.WriteLine($"{nameof(currentTime)}: {currentTime}");
+            //Console.WriteLine($"{nameof(today)}: {today}");
+            //Console.WriteLine($"{nameof(someDate)}: {someDate}");
+            //Console.WriteLine($"{nameof(someMoment)}: {someMoment}");
+            //Console.WriteLine($"{nameof(tomorrow)}: {tomorrow}");
+            //Console.WriteLine($"{nameof(yesterday)}: {yesterday}");
+            //Console.WriteLine($"{nameof(someDay)}: {someDay}");
+
+            #endregion
+
+            #region Formatting Dates and Times
+
+            // --------------------------- part 1 ---------------------------
+
+            //DateTime thisDate1 = new DateTime(2011, 6, 10);
+            //Console.WriteLine("Today is " + thisDate1.ToString("MMMM dd, yyyy") + ".");
+
+            //DateTimeOffset thisDate2 = new DateTimeOffset(2011, 6, 10, 15, 24, 16,
+            //                                              TimeSpan.Zero);
+            //Console.WriteLine("The current date and time: {0:MM/dd/yy H:mm:ss zzz}",
+            //                   thisDate2);
+
+
+            // The example displays the following output:
+            //    Today is June 10, 2011.
+            //    The current date and time: 06/10/11 15:24:16 +00:00
+
+            // --------------------------- part 2 ---------------------------
+
+            //string[] dateValues = { "30-12-2011", "12-30-2011",
+            //                  "30-12-11", "12-30-11" };
+            //string pattern = "MM-dd-yy";
+            //DateTime parsedDate;
+
+            //foreach (var dateValue in dateValues)
+            //{
+            //    if (DateTime.TryParseExact(dateValue, pattern, null,
+            //                              DateTimeStyles.None, out parsedDate))
+            //        Console.WriteLine("Converted '{0}' to {1:d}.",
+            //                          dateValue, parsedDate);
+            //    else
+            //        Console.WriteLine("Unable to convert '{0}' to a date and time.",
+            //                          dateValue);
+            //}
+
+            // --------------------------- additional reference ---------------------------
+            // for more information on how dates can be parsed, please visit the following link
+            // https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx
+
+            #endregion
+
+            #region Getting to Parts of Dates and Times
+
+            //var someTime = new DateTime(2016, 7, 1, 11, 10, 9); // 1 July 2016 11:10:09 AM
+            //int year = someTime.Year; // 2016
+            //int month = someTime.Month; // 7
+            //int day = someTime.Day; // 1
+            //int hour = someTime.Hour; // 11
+            //int minute = someTime.Minute; // 10
+            //int second = someTime.Second; // 9
+
+            //Console.WriteLine($"{nameof(someTime)}: {someTime}");
+            //Console.WriteLine($"{nameof(year)}: {year}");
+            //Console.WriteLine($"{nameof(month)}: {month}");
+            //Console.WriteLine($"{nameof(day)}: {day}");
+            //Console.WriteLine($"{nameof(hour)}: {hour}");
+            //Console.WriteLine($"{nameof(minute)}: {minute}");
+            //Console.WriteLine($"{nameof(second)}: {second}");
+
+            #endregion
+
+            #region Calculating Durations Between DateTimes
+
+            //DateTime nextYear = new DateTime(DateTime.Today.Year + 1, 1, 1);
+            //TimeSpan duration = nextYear - DateTime.Today;
+            //Console.WriteLine($"There are {duration.TotalDays} days left in the year");
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
--- 04_DecisionMaking/04_DecisionMaking/Program.cs
+++ 04_DecisionMaking/04_DecisionMaking/Program.cs
@@ -6,7 +6,186 @@ namespace _04_DecisionMaking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region If This Then That
+            Header("The bool simple type");
+
+            //? The bool keyword is an alias of System.Boolean. It is used to 
+            //?     declare variables to store the Boolean values: true and false.
+            bool someCondition = false;
+
+            //? This creates a variable. The value type is an integer, so sum becomes an
+            //?     strongly typed int variable when we instantiate it.
+
+            //? After that, we use the boolean that we set to false above and if it is true
+            //?     the program will run what is inside of the "if" statement. If it is false,
+            //?     the code inside the if statement is false. Try changing the someCondition
+            //?     bool to be true and false and see the difference in the output.
+            var sum = 10;
+            if (someCondition)
+            {
+                Console.WriteLine("The condition was true, so this is code is executed.");
+                sum = sum + 5;
+                sum = sum * 2;
+            }
+            Console.WriteLine(sum);
+
+            Footer();
+            #endregion
+
+            #region Or Else!
+            Header("Or Else Statement");
+
+
+
+            //? This shows that if we have more than one line in a conditional, we need to 
+            //?     put our code inside of {} brackets.
+            //? Try changing what the value of answer is and see the results in the console.
+
+            Console.WriteLine("What is the capital of Ohio?");
+            Console.WriteLine();
+            var answer = "Columbus"; //! please note that we use the .ToLower to ensure user input is processed
+            if (answer.ToLower() == "columbus")
+            {
+                Console.WriteLine($"The answer given was {answer}.");
+                Console.WriteLine("This is CORRECT!");
+            }
+            else
+            {
+                Console.WriteLine($"The answer given was {answer}.");
+                Console.WriteLine("Sorry, that's not the right answer.");
+            }
+
+            Footer();
+            #endregion
+
+            #region Switch Statements
+            Header("Switch Statements");
+
+            uint guessedNumber = 1; // try different numbers to see resulting behavior
+            switch (guessedNumber)
+            {
+                case 0:
+                    Console.WriteLine("Sorry, 0 is not a valid guess.");
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("You guessed low.");
+                    break;
+                case 4:
+                    Console.WriteLine("You guessed the right number!");
+                    break;
+                default:
+                    Console.WriteLine("You guessed high.");
+                    break;
+            }
+
+            Footer();
+            #endregion
+
+
+            #region Logical Expressions
+            #region Comparison Operators
+            Header("Comparison Operators");
+            DisplayLogicalExpressions();
+
+            int x = 15;
+            int y = 10;
+            Console.WriteLine(x < 10);
+            Console.WriteLine(y >= 0);
+
+            Footer();
+            #endregion
+
+            #region Conditional Logical 
+            Header("Conditional Logical");
+
+            Console.WriteLine(String.Format("true && true   = {0}",true && true));    // true
+            Console.WriteLine(String.Format("true && false  = {0}", true && false));   // false
+            Console.WriteLine(String.Format("false && false = {0}", false && false));  // false
+            Console.WriteLine(String.Format("true || true   = {0}", true || true));    // true
+            Console.WriteLine(String.Format("true || false  = {0}", true || false));   // true
+            Console.WriteLine(String.Format("false || false = {0}", false || false));  // false
+            Console.WriteLine(String.Format("true ^ true    = {0}", true ^ true));     // false
+            Console.WriteLine(String.Format("true ^ false   = {0}", true ^ false));    // true
+            Console.WriteLine(String.Format("false ^ false  = {0}", false ^ false));   // false
+            Console.WriteLine(String.Format("!true  = {0}", !true));           // false
+            Console.WriteLine(String.Format("!false = {0}", !false));          // true
+
+
+            int a = 5;
+            int b = 10;
+            Console.WriteLine();
+            Console.WriteLine($"Example: a = {a}, b = {b}, conditional = (a < b) && (b < 20)");
+            if ((a < b) && (b < 20))
+            {
+                Console.WriteLine("The entire condition is true, so we execute the code inside the {}");
+            }
+
+            Footer();
+            #endregion
+
+            #region Flags
+            Header("Flags");
+            int z = 10;
+            bool isPositive = z > 0;
+            if (isPositive)
+            {
+                Console.WriteLine("The FLAG is set to true!");
+            }
+            Footer();
+            #endregion
+
+            #endregion
+        }
+
+        private static void DisplayLogicalExpressions()
+        {
+            Console.WriteLine("==  // equal");
+            Console.WriteLine("!=  // not equal");
+            Console.WriteLine(">   // greater than");
+            Console.WriteLine("<   // less than");
+            Console.WriteLine(">=  // greater than or equal");
+            Console.WriteLine("<=  // less than or equal");
+        }
+
+        private static void DisplayConditionalLogic()
+        {
+            Console.WriteLine("&&  // logical AND");
+            Console.WriteLine("||  // logical OR");
+            Console.WriteLine("!   // logical NOT (often read as 'bang')");
+            Console.WriteLine("^   // logical XOR (exclusive OR)");
+            Console.WriteLine();
+            Console.WriteLine("true && true    // true");
+            Console.WriteLine("true && false   // false");
+            Console.WriteLine("false && false  // false");
+            Console.WriteLine();
+            Console.WriteLine("true || true    // true");
+            Console.WriteLine("true || false   // true");
+            Console.WriteLine("false || false  // false");
+            Console.WriteLine();
+            Console.WriteLine("true ^ true     // false");
+            Console.WriteLine("true ^ false    // true");
+            Console.WriteLine("false ^ false   // false");
+            Console.WriteLine();
+            Console.WriteLine("!true           // false");
+            Console.WriteLine("!false          // true");
+            Console.WriteLine();
+        }
+
+        private static void Footer()
+        {
+            // add two lines
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void Header(string header)
+        {
+            // add a header and an additional line.
+            Console.WriteLine("-----------------------------" + header + "-----------------------------");
+            Console.WriteLine();
         }
     }
 }
