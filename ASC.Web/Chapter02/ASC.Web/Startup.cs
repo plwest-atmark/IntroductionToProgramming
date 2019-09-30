4275bc0d16297e30df2f02c1070de268a1a4a1c5
plwest.atmark <plwest.atmark@gmail.com>
2019-09-30 15:22:44 +0900

    Updated appsettings for connectionstring

--- ASC.Web/Chapter02/ASC.Web/Startup.cs
+++ ASC.Web/Chapter02/ASC.Web/Startup.cs
@@ -40,6 +40,7 @@ namespace ASC.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
